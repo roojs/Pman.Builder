197ac561aa133e0a4f64317455cd06e608c277bb
alan <alan@alanfast.akbkhome.com>
2010-04-23 14:09:01 +0800

    Pman.Dialog.BuilderAppEdit.js

--- Pman.Dialog.BuilderAppEdit.js
+++ Pman.Dialog.BuilderAppEdit.js
@@ -1,6 +1,9 @@
 //<script type="text/javascript">
-
-// Auto generated file - created by Builder Module - do not edit directly
+//
+// Version 20100423-140831
+//
+// Auto generated file - do not edit directly
+// created by Application Builder (Web version)
 Pman.Dialog.BuilderAppEdit = {
 
     dialog : false,
@@ -18,6 +21,7 @@ Pman.Dialog.BuilderAppEdit = {
         if (this.form) {
            this.form.reset();
            this.form.setValues(data);
+           this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
         }
 
     },
@@ -29,7 +33,7 @@ Pman.Dialog.BuilderAppEdit = {
             xtype : 'LayoutDialog',
             background : true,
             width : 500,
-            height : 165,
+            height : 200,
             title : "Edit \/ Create Module",
             listeners : {
                 show: function (_self)
@@ -103,6 +107,12 @@ Pman.Dialog.BuilderAppEdit = {
                                     allowDecimals : false,
                                     boxLabel : 'Check to enable PUT to Dav after save',
                                     inputValue : 1
+                                },
+                                {
+                                    name : 'gitpath',
+                                    fieldLabel : "Git Path",
+                                    width : 350,
+                                    xtype : 'TextField'
                                 }
                             ]
                         }
@@ -142,3 +152,21 @@ Pman.Dialog.BuilderAppEdit = {
         });
     }
 };
+/*
+--SOURCE--
+QlpoNDFBWSZTWbJJD0AAA8rfgGgAUP36Gj/O346/79/+UANmC282nbrMrCSQqafqmZTwQ000hoGjEAAD
+TQAJJJkBD1T1NND1PU0MgAeoANPUaAAk1JBAp5qIZoTNQGgAaANAGmhzAJgJkYARiYmEwmCGmJpgJIk0
+1GU2o9TyTJo0GQaBoAAAMjwBeQAwXXC7MurD3edXp4XGG1vk3BlhCDENXPH1xdsxE+qvqPvVq1vgS9L3
+/E7a1lqhXY84oRt28V2kpYwGypG30rKoKNY2yjLhb8Wxf/GuIQ4OwGeFnZRhoqrgmqENrDasViIwZFSA
+odUGSmFQUVBiKHXsM/ksZb3Lz46t+/ZTVYGxDS2zHHzVzs0uiu/TVwZtV0LE1mFA8lWsygpgsEYUyktF
+SktJ3c2rMBzXSoVRr3rGFiTdnlNLNXoc0w9aRo3YCSC+lUyaBewW3DTmcK4fOZ1ssDJhepKmPLQcGIH3
+VMzjQzqGd03j5FXU597lDPPnsVccMN3g6DxzxY2lhRtO4SjJlocmMmInRWAx2Ozs+dGLj0bwSWkbJ6X8
+f8sGgDnkj6KhWzHrRt3T+6a6rjst3Ncj7mxkgVYS5SXh2bQla2ntidCvuSJEh2LR5BLS9MxSprpyUbDA
+HvyUEyjKoYBfCndKDoX7VXpo4TfYVtqUypelrMrSbjhxEVTLqmay8e87XSaubIjGsaVSCtEY0yIU4w9m
+VAKMFMdEY10GlbeKCCq5FiK0WuFYBB80bSWkwCzkkw9JUY6b9QyIUEfuw7w5FyBnoxqtWzZZKRPGSkFN
+YTI/ry69pnhmM/Z1RjMybMUjrdVoxk4pluVS3zBQmMM6CgqBU42SIUygBqCfMS3DbnSheY59OqH5AKkR
+jVBgaKtgS628BVyhLX3DXzUHMbIKhPCrhx2H4DXwMBIltDANwZgrMmVgi8tJH1lpQQcXrglZNWs/mOye
+1gPYonfVDJuSDtjRx7qtFYZCukFaFeBlah6wUcQlRyxRq50E5ZU8ysugrPhwxZR5FsFW8zTHDRMQZEAU
+dxignMoaolHfQtg2ilYU7mCVS7aFyuwVg34pkUixGEjingYbhl2ERYiOEnnaSvoihTvY4wlZ9hoPJ3YY
+4A4w2pQGiQqdNCDinXR675awy+9H/i7kinChIWSSHoA=
+*/
