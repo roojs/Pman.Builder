8c38f0a03ef026f00196952f780b5922ad49a000
Alan Knowles <alan@akbkhome.com>
2010-05-25 17:51:02 +0800

    Dump.php

---
+++ Dump.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'Pman.php';
+
+class Pman_Builder_Dump extends Pman
+{
+  
+    // getAuth - everyone allowed in...
+    function getAuth() {
+        return true;
+    }
+  
+    function get($app) {
+        $b = DB_DataObject::Factory('Builder');
+        $b->find(true);
+        
+        
+        
+    }
+    
+}
