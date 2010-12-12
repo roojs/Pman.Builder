3e2dda556a978b513e590767454e6d7cd7005a57
Alan Knowles <alan@akbkhome.com>
2010-12-12 16:23:10 +0800

    old/Builder.readers.js

---
+++ old/Builder.readers.js
@@ -0,0 +1,30 @@
+//<script type="text/javascript">
+
+Pman.Readers.Builder = {
+	root : 'data',
+	totalProperty : 'total',
+	id : 'id',
+	xtype : 'JsonReader',
+	fields : [
+		{name : 'id',type : 'int'},
+		'name',
+		'json',
+		'btype',
+		'app',
+		'module'
+	]
+};
+
+Pman.Readers.Builder_app = {
+	root : 'data',
+	totalProperty : 'total',
+	id : 'id',
+	xtype : 'JsonReader',
+	fields : [
+		{name : 'id',type : 'int'},
+		'app',
+		'davurl',
+		{name : 'davwrite',type : 'int'},
+        'gitpath'
+	]
+};
