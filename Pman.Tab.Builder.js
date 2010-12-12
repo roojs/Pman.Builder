0b398da9b5ded108962268447632da14f0d2679c
Alan Knowles <alan@akbkhome.com>
2010-12-12 15:46:40 +0800

    Pman.Tab.Builder.js

--- Pman.Tab.Builder.js
+++ Pman.Tab.Builder.js
@@ -1,1 +1,40 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        modKey : '750-Pman.Tab.Builder',
+        module : Pman.Tab.Builder,
+        region : 'center',
+        parent : Pman,
+        name : "Pman.Tab.Builder",
+        disabled : false, 
+        permname: 'Builder' 
+    });
+});
+
+Pman.Tab.Builder = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'NestedLayoutPanel',
+            xns: Roo
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
