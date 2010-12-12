87aaf58ef4d89e50c22afb817f308f1b59c98cee
Alan Knowles <alan@akbkhome.com>
2010-12-12 22:03:19 +0800

    Pman.Tab.BuilderTop.js

--- Pman.Tab.BuilderTop.js
+++ Pman.Tab.BuilderTop.js
@@ -381,7 +381,8 @@ Pman.Tab.BuilderTop = new Roo.util.Observable({
                             items : [
                                 {
                                     xtype: 'Item',
-                                    xns: Roo.menu
+                                    xns: Roo.menu,
+                                    text : ""
                                 }
                             ]
                         }
