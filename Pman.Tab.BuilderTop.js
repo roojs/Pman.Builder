28cbffc2e0f7f3bd9496685a74bc88823180f405
Alan Knowles <alan@akbkhome.com>
2010-12-12 21:21:08 +0800

    Pman.Tab.BuilderTop.js

--- Pman.Tab.BuilderTop.js
+++ Pman.Tab.BuilderTop.js
@@ -1,1 +1,40 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        modKey : '001-Pman.Tab.BuilderTop',
+        module : Pman.Tab.BuilderTop,
+        region : 'north',
+        parent : Pman.Tab.BuilderTab,
+        name : "Pman.Tab.BuilderTop",
+        disabled : false, 
+        permname: '' 
+    });
+});
+
+Pman.Tab.BuilderTop = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'ContentPanel',
+            xns: Roo
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
