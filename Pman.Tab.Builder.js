7f068a511fdf0421af4b7c05e55392422963d5aa
Alan Knowles <alan@akbkhome.com>
2010-12-12 21:00:26 +0800

    Pman.Tab.Builder.js

--- Pman.Tab.Builder.js
+++ Pman.Tab.Builder.js
@@ -8,13 +8,13 @@
 Pman.on('beforeload', function()
 {
     Pman.register({
-        modKey : '750-Pman.Tab.Builder',
+        modKey : '001-Pman.Tab.Builder',
         module : Pman.Tab.Builder,
         region : 'center',
         parent : Pman,
-        name : "Pman.Tab.Builder",
+        name : "Builder",
         disabled : false, 
-        permname: 'Builder' 
+        permname: '' 
     });
 });
 
@@ -36,67 +36,21 @@ Pman.Tab.Builder = new Roo.util.Observable({
             listeners : {
                 activate : function (_self)
                 {
-                    if (_this.keyListener) {
-                        return;
-                    }
-                    _this.keyListener = true;
-                    
-                    Roo.get(document).on(   'keyup',  function(e) {
-                        if (!  _this.panel.active ) {
-                            return;
-                        }
-                         
-                        // check..
-                        if (!Pman.Tab.BuilderPanel.isMouseOver && !Pman.Tab.BuilderTree.isMouseOver) {
-                            return;
-                        }
-                       // console.log(e);
-                        //Roo.EventObject.DELETE,
-                        if (e.getCharCode() == Roo.EventObject.DELETE) {
-                            //  console.log('delete');
-                          
-                            var res = Pman.Tab.BuilderTree.deleteCurrent();
-                            if (res) {
-                                 var bp = Pman.Tab.BuilderPanel;
-                                 bp.redraw.defer(100,bp, [ true ]);
-                                
-                            }
-                            e.stopEvent();
-                        }
-                        
-                         
-                     });
-                 
+                   _self.layout.getRegion('center').showPanel(0)
                 }
             },
-            title : "Customize",
+            background : true,
+            fitToFrame : true,
+            region : 'center',
+            title : "Customize App",
             layout : {
                 xtype: 'BorderLayout',
                 xns: Roo,
-                north : {
-                    xtype: 'LayoutRegion',
-                    xns: Roo,
-                    height : 26
-                },
-                west : {
-                    xtype: 'LayoutRegion',
-                    xns: Roo,
-                    split : true,
-                    width : 255
-                },
                 center : {
                     xtype: 'LayoutRegion',
-                    xns: Roo
-                },
-                east : {
-                    xtype: 'LayoutRegion',
                     xns: Roo,
-                    collapsed : true,
-                    collapsible : true,
-                    split : true,
-                    title : "Palete",
-                    titlebar : true,
-                    width : 200
+                    titlebar : false,
+                    tabPosition : 'top'
                 }
             }
         });
