c966388774eb4823832cf96cf1d1c7d115824ca9
Alan Knowles <alan@akbkhome.com>
2010-12-12 22:20:17 +0800

    Pman.Tab.BuilderTree.js

--- Pman.Tab.BuilderTree.js
+++ Pman.Tab.BuilderTree.js
@@ -1,1 +1,40 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        modKey : '001-Pman.Tab.BuilderTree',
+        module : Pman.Tab.BuilderTree,
+        region : 'west',
+        parent : Pman.Tab.BuilderTab,
+        name : "Pman.Tab.BuilderTree",
+        disabled : false, 
+        permname: '' 
+    });
+});
+
+Pman.Tab.BuilderTree = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'NestedLayoutPanel',
+            xns: Roo
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
