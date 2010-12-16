da4d936166d90f4f59f0b3eb36f351377eeadf23
Alan Knowles <alan@akkbhome.com>
2010-12-16 21:59:21 +0800

    Pman.Tab.BuilderPalette.js

--- Pman.Tab.BuilderPalette.js
+++ Pman.Tab.BuilderPalette.js
@@ -37,7 +37,7 @@ Pman.Tab.BuilderPalette = new Roo.util.Observable({
                 activate : function() {
                     _this.panel = this;
                     if (_this.grid) {
-                        _this.grid.footer.onClick('first');
+                       _this.grid.footer.ds.load({})
                     }
                 }
             },
