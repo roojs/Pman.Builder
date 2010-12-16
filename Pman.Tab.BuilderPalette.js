9d7bf75f0a0c02ef4333ca5d1b413f33d5d558c8
Alan Knowles <alan@akkbhome.com>
2010-12-16 21:57:00 +0800

    Pman.Tab.BuilderPalette.js

--- Pman.Tab.BuilderPalette.js
+++ Pman.Tab.BuilderPalette.js
@@ -164,14 +164,6 @@ Pman.Tab.BuilderPalette = new Roo.util.Observable({
                     {
                         xtype: 'ColumnModel',
                         xns: Roo.grid,
-                        header : 'Id',
-                        width : 75,
-                        dataIndex : 'id',
-                        renderer : function(v) { return String.format('{0}', v); }
-                    },
-                    {
-                        xtype: 'ColumnModel',
-                        xns: Roo.grid,
                         header : 'Name',
                         width : 200,
                         dataIndex : 'name',
