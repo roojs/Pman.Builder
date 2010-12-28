ccc7bc61daf5882a083871adda4aaf9d229e385b
Alan Knowles <alan@akbkhome.com>
2010-12-28 18:02:57 +0800

    Pman.Tab.BuilderTree.js

--- Pman.Tab.BuilderTree.js
+++ Pman.Tab.BuilderTree.js
@@ -158,7 +158,7 @@ Pman.Tab.BuilderTree = new Roo.util.Observable({
                             ddGroup : 'component',
                             enableDD : true,
                             rootVisible : true,
-                             : function() {
+                            loadTree : function() {
                                 
                             },
                             clearAll : function() {
