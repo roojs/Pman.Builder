daf9617286b5d492015246442de24add8214e07a
Alan Knowles <alan@akbkhome.com>
2010-12-28 17:19:23 +0800

    Pman.Tab.BuilderView.js

--- Pman.Tab.BuilderView.js
+++ Pman.Tab.BuilderView.js
@@ -1,1 +1,40 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        modKey : '001-Pman.Tab.BuilderView',
+        module : Pman.Tab.BuilderView,
+        region : 'center',
+        parent : Pman.Tab.BuilderTab,
+        name : "Pman.Tab.BuilderView",
+        disabled : false, 
+        permname: '' 
+    });
+});
+
+Pman.Tab.BuilderView = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'ContentPanel',
+            xns: Roo
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
