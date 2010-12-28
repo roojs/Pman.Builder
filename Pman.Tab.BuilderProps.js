4fa876351399741e7e1f19bca6250b88240b508d
Alan Knowles <alan@akbkhome.com>
2010-12-28 18:21:45 +0800

    Pman.Tab.BuilderProps.js

--- Pman.Tab.BuilderProps.js
+++ Pman.Tab.BuilderProps.js
@@ -43,7 +43,7 @@ Pman.Tab.BuilderProps = new Roo.util.Observable({
                         _this.grid = grid;
                     }
                 },
-                setSourceFromNode : function() {
+                setSourceFromNode : function(config) {
                    
                         
                     var cfg = {};
