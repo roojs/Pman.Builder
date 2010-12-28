4e0db0e30edec90e67ef994b2c4636cdb09954a4
Alan Knowles <alan@akbkhome.com>
2010-12-28 17:21:33 +0800

    Pman.Tab.BuilderTree.js

--- Pman.Tab.BuilderTree.js
+++ Pman.Tab.BuilderTree.js
@@ -44,7 +44,7 @@ Pman.Tab.BuilderTree = new Roo.util.Observable({
                         listeners : {
                             activate : function (_self)
                             {
-                                this.tree = _self.tree;
+                                _this.tree = _self.tree;
                                 if (_this.hasMouseEvent) {
                                     return;
                                 }
