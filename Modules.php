0b44bb57adebc87eafaa52cf0527d84a2a4fa919
Alan Knowles <alan@akbkhome.com>
2010-12-28 16:49:41 +0800

    Modules.php

--- Modules.php
+++ Modules.php
@@ -15,7 +15,16 @@ class Pman_Builder_Modules extends Pman
     function get()
     {
         // should just list the modules..
+        $this->init();
+        $enabled =  array('Core') ;
+        $enabled = !empty($this->appModules) ?  array_merge($enabled, explode(',',  $this->appModules)) :  $enabled;
         
+        sort($enabled);
+        $ret = array();
+        foreach($enabled as $r) {
+            $ret[] = array('name' => $r);
+        }
+        $this->jdata($ret);
         
     }
   
