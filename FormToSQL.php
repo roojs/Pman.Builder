76aa8b272d743254e5200e866b97487e7e952e1b
Alan Knowles <alan@akbkhome.com>
2011-01-27 17:30:41 +0800

    FormToSQL.php

---
+++ FormToSQL.php
@@ -0,0 +1,22 @@
+<?php
+
+// quick way to build SQL based on a form design..
+// might have uses later...
+
+require_once 'Pman.php'
+class Pman_Builder_FormToSQL extends Pman {
+    
+    function getAuth(){
+        if (!HTML_FlexyFramework::get()->cli) {
+            die("not cli");
+        }
+        return true;
+    }
+    
+    function get($file)
+    {
+        
+    }
+    
+    
+}
