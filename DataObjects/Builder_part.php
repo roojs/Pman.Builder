748cc8a06cfb7d20e84492efa0f3bbd98c587bdb
Alan Knowles <alan@akbkhome.com>
2011-02-10 14:58:07 +0800

    DataObjects/Builder_part.php

--- DataObjects/Builder_part.php
+++ DataObjects/Builder_part.php
@@ -61,7 +61,7 @@ class Pman_Builder_DataObjects_Builder_part extends DB_DataObject
             mkdir($sp, 0755, true);
         }
         $fn = $sp .'/'. $this->id . '-'. $this->name . '-'. date('d-H-i-s') . '.bjs';
-        $this->writeCopy($fn, $roo);
+        file_put_contents($fn $this->json);
     }
     
     
