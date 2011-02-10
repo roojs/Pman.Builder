3127cad602f9a1d5f1a102055451102507054a15
Alan Knowles <alan@akbkhome.com>
2011-02-10 22:43:24 +0800

    Pman.Tab.BuilderTree.js

--- Pman.Tab.BuilderTree.js
+++ Pman.Tab.BuilderTree.js
@@ -116,13 +116,16 @@ Pman.Tab.BuilderTree = new Roo.util.Observable({
                                                         
                                 
                                 },
-                                nodedrop : function (dropEvent)
+                                nodedrop : function (e)
                                 {
+                                    
+                                    Roo.log("Node Drop");
+                                    return;
                                     e.dropNode.setText(
-                                                _this.configToText(e.dropNode.elConfig)
-                                                );
-                                                var bp = Pman.Tab.BuilderPanel;
-                                                bp.redraw.defer(1000, bp, [true]);
+                                        _this.configToText(e.dropNode.elConfig)
+                                        );
+                                        var bp = Pman.Tab.BuilderPanel;
+                                        bp.redraw.defer(1000, bp, [true]);
                                 
                                 },
                                 click : function (node, e)
