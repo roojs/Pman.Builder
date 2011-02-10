a43c2db5f769f21187b54f08f4471db89835d612
Alan Knowles <alan@akbkhome.com>
2011-02-10 22:35:28 +0800

    Pman.Tab.BuilderTree.js

--- Pman.Tab.BuilderTree.js
+++ Pman.Tab.BuilderTree.js
@@ -181,8 +181,9 @@ Pman.Tab.BuilderTree = new Roo.util.Observable({
                                         var drop_rec = e.source.dragData.selections[0];
                                         var drop_xtype = drop_rec.data.name;
                                         var ok_parents = drop_rec.json.parents;
-                                        
+                                        Roo.log("TEST PARENTS: " + ok_parents.join(', '));
                                         var new_parent = this.nodeXtype((e.point == 'append') ? e.target.parentNode :  e.target);
+                                        Roo.log("NEW PARENT: " + new_parent);
                                         
                                         // see if the new_parent is actually in the list of ok_parents
                                         n.cancel = true;
