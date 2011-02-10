7cd9aa47ce7b884f0bb5a3c010232f71877a7256
Alan Knowles <alan@akbkhome.com>
2011-02-10 19:07:57 +0800

    Pman.Tab.BuilderPalette.js

--- Pman.Tab.BuilderPalette.js
+++ Pman.Tab.BuilderPalette.js
@@ -65,7 +65,7 @@ Pman.Tab.BuilderPalette = new Roo.util.Observable({
                         if (!sn) {
                             sn = '*top';
                         }
-                        var cls = 'palete-hide';
+                        var cls = 'pman-builder-palete-hide';
                         var name  = rowconfig.record.data.name;
                         Roo.each(rowconfig.record.data.parents, function(n) {
                             if (n == name || n.split(':').shift() == name) 
