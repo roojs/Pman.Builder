a4a9c413bf7c027dd0666a5738286803d92ba9a5
Alan Knowles <alan@akbkhome.com>
2011-02-10 21:28:11 +0800

    Pman.Tab.BuilderTree.js

--- Pman.Tab.BuilderTree.js
+++ Pman.Tab.BuilderTree.js
@@ -272,8 +272,8 @@ Pman.Tab.BuilderTree = new Roo.util.Observable({
                             },
                             currentNodeType : function() {
                                 if (!this.currentNode) {return ''; }
-                                var xt = this.currentNode.xtype ||  '';
-                                var xns= this.currentNode['|xns'] ||   '';
+                                var xt = this.currentNode.elConfig.xtype ||  '';
+                                var xns= this.currentNode.elConfig['|xns'] ||   '';
                                 xns += xns.length ? '.' : '';
                                 return xns + xt;
                              
