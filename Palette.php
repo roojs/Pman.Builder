8681a316ea73696543053445e5b2638330a0f5a6
Alan Knowles <alan@akbkhome.com>
2011-02-10 17:16:30 +0800

    Palette.php

---
+++ Palette.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+class Pman_Builder_Palette extends Pman 
+{
+    // generic list we do not care who looks at it..
+    function getAuth(
+    {
+        return true;
+    }
+    
+    
+}
