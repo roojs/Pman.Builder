79c6351c4a677ffd098e85722b2dee9443363a38
Alan Knowles <alan@akbkhome.com>
2011-10-24 13:11:24 +0800

    DataObjects/builder.sql

--- DataObjects/builder.sql
+++ DataObjects/builder.sql
@@ -25,4 +25,4 @@ ALTER TABLE builder_part ADD COLUMN module_id int(11)  NOT NULL;
 ALTER TABLE builder_part ADD COLUMN   name varchar(254)  NOT NULL;
 ALTER TABLE builder_part ADD COLUMN   json longtext  NOT NULL;
 
-ALTER TABLE builder_part ADD COLUMN updated DATETIME;
+ALTER TABLE builder_part ADD COLUMN updated DATETIME ;
