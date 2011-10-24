f2812d774dd290d3bf40d534555df72f1c58e324
Alan Knowles <alan@akbkhome.com>
2011-10-24 13:10:41 +0800

    DataObjects/builder.sql

--- DataObjects/builder.sql
+++ DataObjects/builder.sql
@@ -1,48 +1,28 @@
 
 
 
-CREATE TABLE  `builder` (
-  `id` int(11)  NOT NULL AUTO_INCREMENT,
-  `name` varchar(128)  NOT NULL,
-  btype varchar(16)  NOT NULL,
-  `json` longtext  NOT NULL,
-  PRIMARY KEY (`id`)
+
+
+CREATE TABLE builder_modules (
+  id int(11) NOT NULL AUTO_INCREMENT,
+
+  PRIMARY KEY (id)
 );
 
+ALTER TABLE  builder_modules ADD COLUMN   name varchar(128) NOT NULL;
+ALTER TABLE  builder_modules ADD COLUMN   path varchar(254) NOT NULL;
+ALTER TABLE  builder_modules ADD COLUMN   public int(11) NOT NULL DEFAULT '0';
+ALTER TABLE  builder_modules ADD COLUMN database_dsn VARCHAR(254)  NOT NULL DEFAULT '';
 
-ALTER TABLE `builder` ADD COLUMN `app` VARCHAR(64)  NOT NULL DEFAULT '';
-ALTER TABLE `builder` ADD COLUMN `module` VARCHAR(128)  NOT NULL DEFAULT '';
 
-#-- depreciated..
-
-CREATE TABLE `builder_app` (
-  `id` int(11)  NOT NULL AUTO_INCREMENT,
-  `app` varchar(64)  NOT NULL,
-  `davurl` varchar(128)  NOT NULL,
-  `davwrite` int(1)  NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE  builder_part (
+  id int(11)  NOT NULL AUTO_INCREMENT,
+  
+  PRIMARY KEY (id)
 );
-ALTER TABLE builder_app CHANGE COLUMN davwrite davwrite int(2)  NOT NULL DEFAULT 0;
-ALTER TABLE builder_app ADD COLUMN `gitpath` varchar(255)  NOT NULL DEFAULT '';
 
-
-CREATE TABLE `builder_modules` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
-  `path` varchar(254) NOT NULL,
-  `public` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-);
-ALTER TABLE  `builder_modules` ADD COLUMN `database_dsn` VARCHAR(254)  NOT NULL DEFAULT '';
-
-
-CREATE TABLE  `builder_part` (
-  `id` int(11)  NOT NULL AUTO_INCREMENT,
-  `module_id` int(11)  NOT NULL,
-  `name` varchar(254)  NOT NULL,
-  `json` longtext  NOT NULL,
-  PRIMARY KEY (`id`)
-)
-ENGINE = MyISAM;
+ALTER TABLE builder_part ADD COLUMN module_id int(11)  NOT NULL;
+ALTER TABLE builder_part ADD COLUMN   name varchar(254)  NOT NULL;
+ALTER TABLE builder_part ADD COLUMN   json longtext  NOT NULL;
 
 ALTER TABLE builder_part ADD COLUMN updated DATETIME;
