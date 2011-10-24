4a88609d5e6d4ae4503554600c2f4f9797d3a302
Alan Knowles <alan@akbkhome.com>
2011-10-24 12:55:11 +0800

    DataObjects/Builder_modules.php

--- DataObjects/Builder_modules.php
+++ DataObjects/Builder_modules.php
@@ -55,7 +55,7 @@ class Pman_Builder_DataObjects_Builder_modules extends DB_DataObject
                     
                     
                 }
-                
+                $ff->page->jok("Synced");
                 
                 
                 
