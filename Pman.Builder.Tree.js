f3e761bc35daf805a9da06277b1a30ec629c71dc
Alan Knowles <alan@roojs.com>
2011-10-26 10:54:17 +0800

    Pman.Builder.Tree.js

---
+++ Pman.Builder.Tree.js
@@ -0,0 +1,20 @@
+/**
+ * The code that manages the tree...
+ *
+ * used to be inside the Interface, but has proved to be to difficult to manage.
+ *
+ * In principle, simple event handling code is put in the interface, and any hard
+ * lifting is done in nice files...
+ *
+ *
+ *
+ */
+
+Pman.Builder.Tree = {
+    
+    
+    
+    
+    
+    
+}
