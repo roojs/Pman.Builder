26a2b31aaa402c101005853e65ded5790751e47b
Alan Knowles <alan@roojs.com>
2011-10-26 12:24:43 +0800

    Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.js Pman.Tab.BuilderDatabase.js

--- Pman.Tab.BuilderDatabase.js
+++ Pman.Tab.BuilderDatabase.js
@@ -1,1 +1,41 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+
+
+// register the module first
+Pman.on('beforeload', function()
+{
+    Pman.register({
+        part :  ["Builder","Database"],
+        modKey : '001-Pman.Tab.BuilderDatabase',
+        module : Pman.Tab.BuilderDatabase,
+        region : '',
+        parent : false,
+        name : "unnamed module",
+        disabled : false, 
+        permname: '' 
+    });
+});
+
+Pman.Tab.BuilderDatabase = new Roo.util.Observable({
+
+    panel : false,
+    disabled : false,
+    parentLayout:  false,
+
+    add : function(parentLayout, region)
+    {
+
+        var _this = this;
+        this.parentLayout = parentLayout;
+
+        this.panel = parentLayout.addxtype({
+            xtype: 'NestedLayoutPanel',
+            xns: Roo
+        });
+        this.layout = this.panel.layout;
+
+    }
+});
