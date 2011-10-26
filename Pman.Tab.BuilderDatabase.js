358fd6e2d4f9a690e6e2835368da173c7ee21b76
Alan Knowles <alan@roojs.com>
2011-10-26 12:57:14 +0800

    Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.js Pman.Tab.BuilderDatabase.js

--- Pman.Tab.BuilderDatabase.js
+++ Pman.Tab.BuilderDatabase.js
@@ -33,7 +33,8 @@ Pman.Tab.BuilderDatabase = new Roo.util.Observable({
 
         this.panel = parentLayout.addxtype({
             xtype: 'NestedLayoutPanel',
-            xns: Roo
+            xns: Roo,
+            region : 'center'
         });
         this.layout = this.panel.layout;
 
