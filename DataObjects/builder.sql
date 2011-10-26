8cb8a1530db442c9d7f7723761e8470bb9379dcb
Alan Knowles <alan@roojs.com>
2011-10-26 12:15:31 +0800

    DataObjects/builder.sql

--- DataObjects/builder.sql
+++ DataObjects/builder.sql
@@ -24,10 +24,10 @@ CREATE TABLE  builder_part (
 ALTER TABLE builder_part ADD COLUMN module_id int(11)  NOT NULL;
 ALTER TABLE builder_part ADD COLUMN   name varchar(254)  NOT NULL;
 ALTER TABLE builder_part ADD COLUMN   json longtext  NOT NULL;
-ALTER TABLE builder_part SET json longtext  NOT NULL;
-ALTER TABLE builder_part SET jsource longtext  NOT NULL;
+ALTER TABLE builder_part ADD COLUMN jsource longtext  NOT NULL;
+
+ALTER TABLE builder_part ADD COLUMN updated DATETIME ;
 
 ALTER TABLE builder_part ALTER COLUMN json SET DEFAULT '';
 
 
-ALTER TABLE builder_part ADD COLUMN updated DATETIME ;
