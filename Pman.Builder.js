7299de132f34a95a91ab41bca9acc6974333b9bd
Alan Knowles <alan@akbkhome.com>
2011-10-28 14:30:02 +0800

    Pman.Builder.js

--- Pman.Builder.js
+++ Pman.Builder.js
@@ -95,7 +95,7 @@ Pman.Builder = {
                     
                 }
                 if (data && cb) {
-                     cb.call(_this,data);
+                     cb.call(data);
                     // ??? _this.postCode(data);
                 }
 
