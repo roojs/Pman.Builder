63c2add3d585a40894d0a41b5466625e5223af59
Alan Knowles <alan@akbkhome.com>
2011-10-31 11:41:22 +0800

    DataObjects/Builder_part.php

--- DataObjects/Builder_part.php
+++ DataObjects/Builder_part.php
@@ -142,7 +142,7 @@ class Pman_Builder_DataObjects_Builder_part extends DB_DataObject
     {
        // echo '<PRE>';print_R($this);exit;
         $m  = $this->module();
-        $m->gitCommit($this->name . '.bjs', $this->bjs());
+        
         $ret = $this->toArray();
         
         return $ret;
