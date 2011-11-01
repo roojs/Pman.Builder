9495c8a00929e5848b650777e02297c0c1b9fd97
Alan Knowles <alan@roojs.com>
2011-11-01 10:15:25 +0800

    DataObjects/builder.sql

--- DataObjects/builder.sql
+++ DataObjects/builder.sql
@@ -31,15 +31,16 @@ ALTER TABLE builder_part ALTER COLUMN json SET DEFAULT '';
 
 CREATE TABLE  builder_tables (
   id int(11)  NOT NULL AUTO_INCREMENT,
- 
   PRIMARY KEY (id),
-  INDEX lookup(parent_id, name)
+  
 );
 
 ALTER TABLE builder_tables ADD COLUMN name varchar(128)  NOT NULL;
 ALTER TABLE builder_tables ADD COLUMN descrip varchar(254)  NOT NULL;
 ALTER TABLE builder_tables ADD COLUMN parent_id int(11)  NOT NULL;
 ALTER TABLE builder_tables ADD COLUMN dbschema text  NOT NULL;
+
+ALTER TABLE builder_tables ADD INDEX lookup(parent_id, name);
 
 ALTER TABLE builder_tables COMMENT = 'list of tables (with relations ships and grouping)';
 COMMENT ON TABLE builder_tables  IS  'list of tables (with relations ships and grouping)';
