bf30fd333bfdc645372d74a340f0cc8fab2ceeba
Alan Knowles <alan@roojs.com>
2011-11-01 10:53:29 +0800

    Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.js Pman.Tab.BuilderDatabase.js

--- Pman.Tab.BuilderDatabase.js
+++ Pman.Tab.BuilderDatabase.js
@@ -76,6 +76,7 @@ Pman.Tab.BuilderDatabase = new Roo.util.Observable({
                             loader : {
                                 xtype: 'TreeLoader',
                                 xns: Roo.tree,
+                                root : 'data',
                                 baseParams : { _tree: 1, 'sort': 'name', dir: 'ASC' },
                                 dataUrl : baseURL + '/Roo/Builder_tables.php'
                             },
