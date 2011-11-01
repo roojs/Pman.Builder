e657a5407aa804c03e3dcfa8049b47df8b093f5e
Alan Knowles <alan@roojs.com>
2011-11-01 11:31:53 +0800

    Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.js Pman.Tab.BuilderDatabase.js

--- Pman.Tab.BuilderDatabase.js
+++ Pman.Tab.BuilderDatabase.js
@@ -34,6 +34,7 @@ Pman.Tab.BuilderDatabase = new Roo.util.Observable({
         this.panel = parentLayout.addxtype({
             xtype: 'NestedLayoutPanel',
             xns: Roo,
+            background : true,
             region : 'center',
             title : "Manage Database",
             layout : {
