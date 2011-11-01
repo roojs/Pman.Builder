3f2bddc92f1168d8d03f4ffae26d07f48d8e13b2
Alan Knowles <alan@roojs.com>
2011-11-01 10:25:50 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -84,6 +84,25 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
     }
     
     
+    function tableSchema($tn)
+    {
+        $desc[$tn] = array();
+        $dd = clone($do);
+        
+       // DB_DataObject::DebugLevel(1);
+        $dd->query("SELECT
+                c.column_name as name,
+                pgd.description as desc
+            FROM pg_catalog.pg_statio_all_tables as st
+                inner join pg_catalog.pg_description pgd on (pgd.objoid=st.relid)
+                inner join information_schema.columns c on (pgd.objsubid=c.ordinal_position and c.table_schema=st.schemaname and c.table_name=st.relname)
+            WHERE
+                c.table_schema = 'public' and c.table_name = '{$tn}'
+        ");
+        while($dd->fetch()) {
+            $desc[$tn][$dd->name] = $dd->desc;
+        }
+    }
     
     
 }
