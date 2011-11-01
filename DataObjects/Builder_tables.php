3de882524e38bf8024725dad18507c619256d2f4
Alan Knowles <alan@roojs.com>
2011-11-01 10:21:45 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -32,11 +32,50 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
     
     function syncDatabase()
     {
+        global $_DB_DATAOBJECT;
         $x = DB_DataObject::factory('builder_tables');
         $mine = $this->fetchAll('id', 'name');
         
+        // ensure everything is loaded...
+        $tq = DB_DataObject::factory('builder_tables');
+        $tq->table();
+        $tq->links();
+        $tables = $_DB_DATAOBJECT['INI'][$tq->_database];
+        
+         
+        $ret = array();
+        $t = array_keys($tables);
+        sort($t);
+        
+        // for postgres we can get descriptions - this should just fail in Mysql..
+        $desc= array();
+       // DB_DataObjecT::DebugLevel(1);
+        $tq = DB_DataObject::factory('Person');
+        $tq->query( "
+            select relname, obj_description( oid) as desc FROM pg_catalog.pg_class");
+        while ($tq->fetch()) {
+            $desc[$tq->relname] = $tq->desc;
+        }
+
+            
+            
         
         
+        
+        foreach( $t as $k) {
+            if (preg_match('/__keys$/', $k)) {
+                continue;
+            }
+            $do = DB_DataObject::factory($k);
+            if (!is_a($do,'DB_DataObject')) {
+                continue;
+            }
+            $ret[] = array(
+                    'name' => $k,
+                    'desc' => isset($desc[$k]) ? $desc[$k] : ''
+                );
+        }
+            
         
         
     }
