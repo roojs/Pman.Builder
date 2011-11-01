cb8775ca00c6840af51335c7e56ec935df73092f
Alan Knowles <alan@roojs.com>
2011-11-01 10:18:35 +0800

    DataObjects/Builder_tables.php

---
+++ DataObjects/Builder_tables.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Table Definition for builder_tables
+ */
+require_once 'DB/DataObject.php';
+
+class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    public $__table = 'builder_tables';      // table name
+    public $id;                              // int(11)  not_null primary_key auto_increment
+    public $name;                            // string(128)  not_null
+    public $descrip;                         // string(254)  not_null
+    public $parent_id;                       // int(11)  not_null multiple_key
+    public $dbschema;                        // blob(65535)  not_null blob
+
+    
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
