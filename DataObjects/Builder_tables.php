2e0ed9a511fa0bb75ad865123e6d6ccaeecbea2c
Alan Knowles <alan@roojs.com>
2011-11-02 12:38:13 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -83,7 +83,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
             // assumes '_' is not used in module name.
             $mod = array_pop(
                     explode('_',
-                        substring(get_class($do), 0, -1 * (strlen('_DataObject_') + strlen($k)))
+                        substr(get_class($do), 0, -1 * (strlen('_DataObject_') + strlen($k)))
                     ));
             // should get 'ZZZ' part.. : XXX_ZZZZ_DataObject_xx_Builder
             if (!isset($modids[$mod])) {
