1a5dece426f5bbbd7415c624998d42b766df06d2
Alan Knowles <alan@akbkhome.com>
2011-11-02 17:34:17 +0800

    Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.js Pman.Tab.BuilderDatabase.js

--- Pman.Tab.BuilderDatabase.js
+++ Pman.Tab.BuilderDatabase.js
@@ -192,7 +192,7 @@ Pman.Tab.BuilderDatabase = new Roo.util.Observable({
                                         var pa = this.editNode.parentNode.attributes;
                                         Roo.log(pa);
                                         var pr =  {
-                                            id : attr.id < 0 ? 0 : attr.id, 
+                                     
                                             parent_id : pa.id,
                                             descrip : this.editNode.text,
                                             name : ''
