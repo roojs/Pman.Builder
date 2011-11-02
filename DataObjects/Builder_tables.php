c87a944b569f87387f21347e304b1011e5aa2960
Alan Knowles <alan@roojs.com>
2011-11-02 12:30:51 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -35,7 +35,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
     {
         global $_DB_DATAOBJECT;
         $x = DB_DataObject::factory('builder_tables');
-        $x->whereAdD("name != ''"); // real tables only..
+        $x->whereAdd("name != ''"); // real tables only..
         $mine = $this->fetchAll('name', 'id');
         
         // ensure everything is loaded...
