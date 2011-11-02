481815b20e073400cce6b5991fffa58ed29ca623
Alan Knowles <alan@akbkhome.com>
2011-11-02 17:49:04 +0800

    Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.bjs Pman.Tab.BuilderDatabase.js Pman.Tab.BuilderDatabase.js

--- Pman.Tab.BuilderDatabase.js
+++ Pman.Tab.BuilderDatabase.js
@@ -126,6 +126,11 @@ Pman.Tab.BuilderDatabase = new Roo.util.Observable({
                                    _this.treepanel.menu = Roo.factory(_this.treepanel.menu);
                                    _this.treepanel.menu.show(node.ui.anchor,'tr');
                                    _this.cxnode = node;
+                                },
+                                beforenodedrop : function (dropEvent)
+                                {
+                                    Roo.log(dropEvent);
+                                    
                                 }
                             },
                             containerScroll : true,
