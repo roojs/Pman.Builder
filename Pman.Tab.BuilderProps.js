80d7d578f7f0b816298a2cbf7136aa03769d74c4
Alan Knowles <alan@akbkhome.com>
2011-11-04 13:42:02 +0800

    Pman.Tab.BuilderProps.bjs Pman.Tab.BuilderProps.bjs Pman.Tab.BuilderProps.js Pman.Tab.BuilderProps.js

--- Pman.Tab.BuilderProps.js
+++ Pman.Tab.BuilderProps.js
@@ -92,7 +92,7 @@ Pman.Tab.BuilderProps = new Roo.util.Observable({
                         if (v == 'true') {
                             v = true;
                         }
-                        var ec = _this.grid.currentNode.elConfig.listeners ;
+                        var ec = _this.grid.currentNode.elConfig;
                         if (id[0] == '!') {
                             ec.listeners = ec.listeners || {};
                             ec.listeners[id.substring(1)] = v;
