2cbcd4afbde985328d248e827ad08be4cf403c0d
Alan Knowles <alan@akbkhome.com>
2011-11-12 14:07:40 +0800

    Pman.Dialog.BuilderForm.bjs Pman.Dialog.BuilderForm.bjs Pman.Dialog.BuilderForm.js Pman.Dialog.BuilderForm.js

--- Pman.Dialog.BuilderForm.js
+++ Pman.Dialog.BuilderForm.js
@@ -55,7 +55,7 @@ Pman.Dialog.BuilderForm = {
                             {
                                 xtype: 'ColumnModel',
                                 xns: Roo.grid,
-                                header : 'Source Data'
+                                header : 'Source Field'
                             },
                             {
                                 xtype: 'ColumnModel',
