cbdf5e0ac5348487e795a1a2201dee1391a96c24
Alan Knowles <alan@roojs.com>
2014-02-06 18:15:39 +0800

    Preview.php

---
+++ Preview.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * extends the CMS preview  - but should take the template from the builder..
+ * intended use is with the bootstrap code HTML generated..
+ *
+*/
+
+class Pman_Builder_Preview extends Pman_Cms_Preview
+{
+    
+    
+    
+}
