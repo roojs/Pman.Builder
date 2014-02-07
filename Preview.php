4113d5e96a23ef63aca659dc8457eba725a466f8
Alan Knowles <alan@roojs.com>
2014-02-07 11:03:28 +0800

    Preview.php

--- Preview.php
+++ Preview.php
@@ -15,6 +15,10 @@ class Pman_Builder_Preview extends Pman_Cms_Preview
         //var_dump($name, $default);
         //DB_DataObject::debugLevel(5);
         parent::loadPage($prefix, $name, $default );
+        
+        
+        // template...
+        
         // menus????
         
          
@@ -22,10 +26,26 @@ class Pman_Builder_Preview extends Pman_Cms_Preview
     
     function outputBody()
     {
+        
+        $oo = HTML_FlexyFramework::get()->Pman_Cms;
+        $template_prefix = isset($oo['template_prefix']) ? $oo['template_prefix'] : '';
+       
+        $m = DB_DAtaObject::Factory('builder_modules');
+        $m->get($template_prefix);
+        
         // needs to modify the template directory??
         // use the builder_module == app name
         // look for part with same name.
-        return parent::outputBody();
+        $template_engine = new HTML_Template_Flexy(array(
+            'template_dir' => $m->path
+        ));
+        $template_engine->compile($this->template);
+        if ($this->elements) { /* BC crap! */
+            $this->elements = HTML_Template_Flexy_Factory::setErrors($this->elements,$this->errors);
+        }
+        $template_engine->elements = $this->elements;
+        $template_engine->outputObject($this,$this->elements);
+        
         
         
     }
