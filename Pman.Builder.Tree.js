94857277ea75a76ee7832a2149b48f4722c24f21
Edward <edward@roojs.com>
2014-03-10 16:44:49 +0800

    Pman.Builder.Tree.js

--- Pman.Builder.Tree.js
+++ Pman.Builder.Tree.js
@@ -307,6 +307,8 @@ Pman.Builder.Tree = {
     },
     loadTree : function(o)
     {
+        Roo.log('ooo');
+        Roo.log(o);
         var tree = Pman.Tab.BuilderTree.tree;
         this.clearAll();
         tree.root.elConfig = o;
