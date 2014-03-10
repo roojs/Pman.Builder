0fd00525c38e3b3268206a57ce355af71fe29278
Edward <edward@roojs.com>
2014-03-10 16:21:24 +0800

    ERM.php

--- ERM.php
+++ ERM.php
@@ -154,6 +154,7 @@ class Pman_Builder_ERM extends Pman
             }
             
             $defs =  $dd->getDatabaseConnection()->tableInfo($tn);
+            print_r($defs);exit;
             $types[$tn] = array();
             foreach($defs as $c) {
                 $types[$tn][$c['name']] = $c['type'];
