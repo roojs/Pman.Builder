0f8bf898872452208a3c698a36e3b50898c58579
Edward <edward@roojs.com>
2014-03-14 15:50:52 +0800

    Pman.Builder.js

--- Pman.Builder.js
+++ Pman.Builder.js
@@ -68,9 +68,10 @@ Pman.Builder = {
         
 
         var js = Pman.Builder.Tree.toJS();
-        
         var render = new Pman.Builder.JsRender(js); 
          
+         Roo.log('render');
+        Roo.log(render);
         // console.log(js);
         // console.log(json);
         
