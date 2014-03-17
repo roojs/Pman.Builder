87d15372b70ad2993c7416bb0648bf34da39c6fb
Edward <edward@roojs.com>
2014-03-17 15:03:59 +0800

    Pman.Builder.View.js

--- Pman.Builder.View.js
+++ Pman.Builder.View.js
@@ -407,8 +407,8 @@ Pman.Builder.View = {
             var top = new Roo.bootstrap.Body({});
             top.onRender(false,false);
             this.bootstrapModal = top.addxtype(cfg.items[0]).items[0];
-            
-            this.applyFlexy(this.bootstrapModal);
+            Roo.log(this.bootstrapModal)
+            //this.applyFlexy(this.bootstrapModal);
             
             if (cfg.items[0].xtype =='Modal' ) {
                 this.bootstrapModal.show();
