8cf1a4cf52dc4c5f1f4cda724cdc5c6eb1b7087d
Edward <edward@roojs.com>
2014-04-10 14:51:02 +0800

    Pman.Builder.View.js

--- Pman.Builder.View.js
+++ Pman.Builder.View.js
@@ -73,12 +73,9 @@ Pman.Builder.View = {
     toHTML : function ()  {
         var ret = '';
         Roo.select('body > div',true).each(function(el) {
-            Roo.log('toHTML');
-            Roo.log(el);
             this.traverseDOMTree(function(s) { ret+=s; }, el.dom, 1);
             ret+="\n";
         }, this);
-        Roo.log(ret);
         return ret;
         
     },
