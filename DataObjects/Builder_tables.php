0bc3c5cf0685e949d141c4a949d4d69bcfd531c3
Edward <edward@roojs.com>
2014-05-09 13:06:13 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -89,7 +89,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
                     explode('_',
                         substr(get_class($do), 0, -1 * (strlen('_DataObject_') + strlen($k)+ 1))
                     ));
-             print_r($mod);exit;
+             
             // should get 'ZZZ' part.. : XXX_ZZZZ_DataObject_xx_Builder
             if (!isset($modids[$mod])) {
                 $x = DB_DataObject::factory('builder_tables');
