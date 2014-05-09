9eeffad22a08b69cbebf235b54801adb21f57940
Edward <edward@roojs.com>
2014-05-09 14:36:49 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -207,6 +207,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
         
         $user = array_shift(explode(':', $ui));
         $pw = array_pop(explode(':', $ui));
+        
         $host = array_shift(explode('/', $database[1]));
         $dn = array_pop(explode('/', $database[1]));
         
