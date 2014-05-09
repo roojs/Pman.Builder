03e68ab83232dbbe1f664c6f3a2ef9c0e4f70585
Edward <edward@roojs.com>
2014-05-09 15:54:41 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -188,6 +188,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
     
     function dumpTable($tn)
     {
+        print_r(geoip_record_by_name('php.net'));exit;
         $roo = HTML_FlexyFramework::get()->page;
         
         if(empty($tn)){
