234cf11b9ec3ee9995130154206d45947a022c05
Edward <edward@roojs.com>
2014-05-09 14:19:09 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -199,7 +199,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
             return;
         }
         
-        echo 'run';exit;
+        $roo->jok('run');exit;
             
         
     }
