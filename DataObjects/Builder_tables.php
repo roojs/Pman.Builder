b9c46a7fc20234c292f6d9332b300c4b6ae16304
Edward <edward@roojs.com>
2014-05-09 13:02:25 +0800

    DataObjects/Builder_tables.php

--- DataObjects/Builder_tables.php
+++ DataObjects/Builder_tables.php
@@ -54,7 +54,7 @@ class Pman_Builder_DataObjects_Builder_tables extends DB_DataObject
         // for postgres we can get descriptions - this should just fail in Mysql..
         $desc= array();
         $dsn = HTML_FlexyFramework::get()->database;
-        
+        print_r($dsn);exit;
         if (preg_match('/^pgsql:/', $dsn )) {
             $tq = DB_DataObject::factory('builder_tables');
             $tq->query( "
